d8164c4433bf5ce46ab26d271e6094a1b54b6f57
Idynx <nuyim11054@gmail.com>
2026-03-25 16:28:00 +0700

    Add files via upload

---
+++ Lab12/myproject/myapp/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
+from myapp.models import person
+
+def form(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        age = request.POST.get("age")
+
+        person.objects.create(
+            name = name,
+            age = age
+        )
+        return redirect("/")
+    else:
+        return render(request, "form1.html")
+
+def index(request):
+    all_person = person.objects.all()
+    return render(request, 'index1.html', {"all_person": all_person})
+
+def edit(request, person_id):
+    p = get_object_or_404(person, pk=person_id) 
+    
+    if request.method == "POST":
+        name = request.POST.get("name")
+        age = request.POST.get("age")
+
+        p.name = name
+        p.age = age
+        p.save()
+        return redirect("/")
+    else:
+
+        return render(request, "edit.html", {"person": p})
+
+def delete(request, person_id):
+    p = get_object_or_404(person, pk=person_id)
+    p.delete()
+    return redirect("/")
